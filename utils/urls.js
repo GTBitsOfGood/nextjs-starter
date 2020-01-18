29e2f0580e7ba4f1cef792364b29e91786bbf2aa
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-01-18 12:45:45 -0500

    Fixed missing prod url

--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,9 @@
 const prod = process.env.NODE_ENV === "production";
 
 export default {
-  baseUrl: prod ? "" : "http://localhost:3000",
+  baseUrl: prod
+    ? "https://nextjs-starter-flax-sigma.now.sh"
+    : "http://localhost:3000",
   dbUrl: prod
     ? process.env.MONGO_DB
     : process.env.MONGO_DEV_DB || "mongodb://localhost:27017",
