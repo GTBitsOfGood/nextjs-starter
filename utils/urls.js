379be54d842d8047cc96a652822097ead75daeb3
Matt Chen <matt.chen522@gmail.com>
2021-09-29 01:17:57 -0400

    Sample Pull Request (#12)
    * change texgt
    
    * added workflow
    
    * workflow

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,6 @@
 export default {
+  // NOTE: For nextjs apps, instead of BASE_URL, consider using NEXT_PUBLIC_VERCEL_URL
+  // https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
   baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
   dbUrl: process.env.MONGO_DB ?? "mongodb://localhost:27017",
   pages: {
